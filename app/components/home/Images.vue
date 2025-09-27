e3f5700a927887c426409b7819e5d76bc1ea1afc
Maciek <r3cenzialny@gmail.com>
2025-09-28 00:39:40 +0200

    init-3

--- app/components/home/Images.vue
+++ app/components/home/Images.vue
@@ -5,17 +5,17 @@
       <NuxtImg
         src="/1.png"
         alt="Widok aplikacji"
-        class="max-w-2xl w-full rounded shadow-xl"
+        class="lg:max-w-md xl:max-w-xl 2xl:max-w-2xl rounded shadow-xl"
       />
       <NuxtImg
         src="/2.png"
         alt="Widok aplikacji"
-        class="max-w-2xl w-full rounded shadow-xl"
+        class="lg:max-w-md xl:max-w-xl 2xl:max-w-2xl rounded shadow-xl"
       />
       <NuxtImg
         src="/3.png"
         alt="Widok aplikacji"
-        class="max-w-2xl w-full rounded shadow-xl object-cover"
+        class="xl:max-w-xl 2xl:max-w-2xl rounded shadow-xl object-cover"
       />
     </div>
   </div>
